9d316e83cb737f2bcb74082a8a17c6889619ad47
Dave Samojlenko <dsamojlenko@gmail.com>
2018-01-12 17:10:07 -0500

    fix some code formatting

--- modules/custom/pco_cities_ext/login_redirect/src/Form/LoginRedirectConfigForm.php
+++ modules/custom/pco_cities_ext/login_redirect/src/Form/LoginRedirectConfigForm.php
@@ -1,110 +1,82 @@
 <?php
 
-/**
-
- * @file
-
- * Contains \Drupal\login_redirect\Form\LoginRedirectConfigForm.
-
- */
-
 namespace Drupal\login_redirect\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 
 use Drupal\Core\Form\FormStateInterface;
 
-class LoginRedirectConfigForm extends ConfigFormBase {
+class LoginRedirectConfigForm extends ConfigFormBase
+{
+  public function getFormId()
+  {
 
-    /**
+    return 'login_redirect_config';
 
-     * {@inheritdoc}
+  }
 
-     */
+  public function buildForm(array $form, FormStateInterface $form_state)
+  {
 
-    public function getFormId() {
+    $form = parent::buildForm($form, $form_state);
 
-        return 'login_redirect_config';
+    $config = $this->config('login_redirect.settings');
 
+    $roles = user_role_names(TRUE);
+
+    // create an array of text fields for role-specific paths
+    foreach ($roles as $key => $role) {
+      $form['roles[' . $key . ']'] = array(
+        '#type' => 'textfield',
+        '#title' => $this->t($role),
+        '#default_value' => $config->get('login_redirect.' . $key),
+      );
     }
 
-    /**
+    // default path
+    $form['default'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Default'),
+      '#default_value' => $config->get('login_redirect.default'),
+    ];
 
-     * {@inheritdoc}
+    return $form;
 
-     */
+  }
 
-    public function buildForm(array $form, FormStateInterface $form_state) {
+  public function submitForm(array &$form, FormStateInterface $form_state)
+  {
 
-        $form = parent::buildForm($form, $form_state);
+    // get the roles array from the form (couldn't figure out how to do this with $form_state)
+    $roles = $_POST['roles'];
 
-        $config = $this->config('login_redirect.settings');
+    // get the config object
+    $config = $this->config('login_redirect.settings');
 
-        $roles = user_role_names(TRUE);
-
-        // create an array of text fields for role-specific paths
-        foreach ($roles as $key => $role) {
-            $form['roles[' . $key .']'] = array(
-                '#type' => 'textfield',
-                '#title' => $this->t($role),
-                '#default_value' => $config->get('login_redirect.' . $key)
-            );
-        }
-
-        // default path
-        $form['default'] = [
-            '#type' => 'textfield',
-            '#title' => $this->t('Default'),
-            '#default_value' => $config->get('login_redirect.default')
-        ];
-
-        return $form;
-
+    // loop over each provided role and set the path in config
+    foreach ($roles as $role => $path) {
+      $config->set('login_redirect.' . $role, $path);
     }
 
-    /**
+    // set the default value
+    $config->set('login_redirect.default', $form_state->getValue('default'));
 
-     * {@inheritdoc}
+    $config->save();
 
-     */
+    drupal_set_message("Settings saved");
 
-    public function submitForm(array &$form, FormStateInterface $form_state) {
+    return parent::submitForm($form, $form_state);
+  }
 
-        // get the roles array from the form (couldn't figure out how to do this with $form_state)
-        $roles = $_POST['roles'];
+  protected function getEditableConfigNames()
+  {
 
-        // get the config object
-        $config = $this->config('login_redirect.settings');
+    return [
 
-        // loop over each provided role and set the path in config
-        foreach($roles as $role => $path) {
-            $config->set('login_redirect.' . $role, $path);
-        }
+      'login_redirect.settings',
 
-        // set the default value
-        $config->set('login_redirect.default', $form_state->getValue('default'));
+    ];
 
-        $config->save();
-
-        drupal_set_message("Settings saved");
-
-        return parent::submitForm($form, $form_state);
-    }
-
-    /**
-
-     * {@inheritdoc}
-
-     */
-
-    protected function getEditableConfigNames() {
-
-        return [
-
-            'login_redirect.settings',
-
-        ];
-
-    }
+  }
 
 }
