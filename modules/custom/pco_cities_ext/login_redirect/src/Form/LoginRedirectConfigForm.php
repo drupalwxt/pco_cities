39faa01d2c06df072b07ef5b6e506799e1b67e2e
Dave Samojlenko <dsamojlenko@gmail.com>
2018-01-12 17:24:40 -0500

    more formatting fixes

--- modules/custom/pco_cities_ext/login_redirect/src/Form/LoginRedirectConfigForm.php
+++ modules/custom/pco_cities_ext/login_redirect/src/Form/LoginRedirectConfigForm.php
@@ -6,17 +6,13 @@ use Drupal\Core\Form\ConfigFormBase;
 
 use Drupal\Core\Form\FormStateInterface;
 
-class LoginRedirectConfigForm extends ConfigFormBase
-{
-  public function getFormId()
-  {
+class LoginRedirectConfigForm extends ConfigFormBase {
 
+  public function getFormId() {
     return 'login_redirect_config';
-
   }
 
-  public function buildForm(array $form, FormStateInterface $form_state)
-  {
+  public function buildForm(array $form, FormStateInterface $form_state) {
 
     $form = parent::buildForm($form, $form_state);
 
@@ -24,16 +20,15 @@ class LoginRedirectConfigForm extends ConfigFormBase
 
     $roles = user_role_names(TRUE);
 
-    // create an array of text fields for role-specific paths
+    // Create an array of text fields for role-specific paths.
     foreach ($roles as $key => $role) {
-      $form['roles[' . $key . ']'] = array(
+      $form['roles[' . $key . ']'] = [
         '#type' => 'textfield',
         '#title' => $this->t($role),
         '#default_value' => $config->get('login_redirect.' . $key),
-      );
+      ];
     }
 
-    // default path
     $form['default'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Default'),
@@ -44,32 +39,26 @@ class LoginRedirectConfigForm extends ConfigFormBase
 
   }
 
-  public function submitForm(array &$form, FormStateInterface $form_state)
-  {
+  public function submitForm(array &$form, FormStateInterface $form_state) {
 
-    // get the roles array from the form (couldn't figure out how to do this with $form_state)
     $roles = $_POST['roles'];
 
-    // get the config object
     $config = $this->config('login_redirect.settings');
 
-    // loop over each provided role and set the path in config
     foreach ($roles as $role => $path) {
       $config->set('login_redirect.' . $role, $path);
     }
 
-    // set the default value
     $config->set('login_redirect.default', $form_state->getValue('default'));
 
     $config->save();
 
-    drupal_set_message("Settings saved");
+    drupal_set_message($this->t("Settings saved"));
 
     return parent::submitForm($form, $form_state);
   }
 
-  protected function getEditableConfigNames()
-  {
+  protected function getEditableConfigNames() {
 
     return [
 
